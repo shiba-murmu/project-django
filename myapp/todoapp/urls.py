2f7e0ceeb4e85a9b2da99d31e009825af1bfc60a
Shiba Murmu <shibamurmu001@gmail.com>
2025-05-22 21:33:49 +0530

    Add profile view and template; update navigation links in base template

--- myapp/todoapp/urls.py
+++ myapp/todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from todoapp.views import home , login
+from todoapp.views import home , login , profile
 
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('', login, name='login'),
+    path('profile/', profile, name='profile'),
 ]
--- myapp/todoapp/views.py
+++ myapp/todoapp/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    # homepage function views here
     return render(request, 'home.html')
 
 def login(request):
+    # login function
     return render(request, 'login.html')
+
+def profile(request):
+    # profile function
+    return render(request, 'profile.html')
