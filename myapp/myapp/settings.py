45993b879ce2a91e758410fe79093de8ffaebaee
Shiba Murmu <shibamurmu001@gmail.com>
2025-05-24 15:31:04 +0530

    made changes in host allowed

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-q^!qwikaf0%iy(&3ocjy+e_ii-gyyx%aonxi-%6_=xs4qi(m!q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'project-django-production.up.railway.app',
+]
 
 
 # Application definition
